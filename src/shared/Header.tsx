eff90f5b58e87ef1b8fd81c11aa891b81f610521
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-11 20:20:54 +0200

    Favorite count in header

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -3,8 +3,15 @@ import React from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { ImageBackground } from "react-native";
 import { StatusBar } from "react-native";
+import { useSelector } from "react-redux";
+import { store } from "../store";
+
+// TODO
+type RootState = ReturnType<typeof store.getState>;
 
 const Header = ({ title }: HeaderProps) => {
+  const locations = useSelector((state: RootState) => state.meetups);
+
   return (
     <ImageBackground
       source={require("../../assets/background.jpg")}
@@ -13,6 +20,18 @@ const Header = ({ title }: HeaderProps) => {
       <View style={styles.headerTitle}>
         <FontAwesome name="meetup" size={24} color="black" />
         <Text style={styles.headerText}>{title}</Text>
+      </View>
+      <View style={styles.favoriteContainer}>
+        <FontAwesome name="heart" size={24} color="red" />
+        <Text>:</Text>
+        <Text>
+          {locations.reduce((count, location) => {
+            if (location.favorite) {
+              return count + 1;
+            }
+            return count;
+          }, 0)}
+        </Text>
       </View>
     </ImageBackground>
   );
@@ -23,12 +42,12 @@ export default Header;
 const styles = StyleSheet.create({
   header: {
     marginTop: StatusBar.currentHeight,
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
   headerTitle: {
-    height: "100%",
+    // height: "100%",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
@@ -42,6 +61,9 @@ const styles = StyleSheet.create({
   icon: {
     position: "absolute",
     left: 16,
+  },
+  favoriteContainer: {
+    flexDirection: "row",
   },
 });
 
