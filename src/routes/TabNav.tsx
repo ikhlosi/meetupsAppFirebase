981d6e8f6684dafa8d457d69fa9e339b11939e6d
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-11 22:55:21 +0200

    Added tab bar icons

--- src/routes/TabNav.tsx
+++ src/routes/TabNav.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MeetupStack from "./MeetupStack";
 import AboutStack from "./AboutStack";
+import { Ionicons } from "@expo/vector-icons";
 
 // Defining the type of the navigator: what screens it will have and what properties each screen will take. `undefined`: no properties
 type TabParamList = {
@@ -17,9 +18,22 @@ const TabNav = () => {
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="MeetupStack"
-        screenOptions={{
+        screenOptions={({ route }) => ({
           headerShown: false,
-        }}
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap;
+
+            if (route.name === "MeetupStack") {
+              iconName = focused ? "apps" : "apps-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+            } else if (route.name === "AboutStack") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+            }
+          },
+        })}
       >
         <Tab.Screen name="MeetupStack" component={MeetupStack} />
         <Tab.Screen name="AboutStack" component={AboutStack} />
